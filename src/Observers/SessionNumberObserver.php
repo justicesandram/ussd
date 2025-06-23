6baf0ff1233786588ce7caab91bbbfe9805df997
Justice Sandram <108130114+justicesandram@users.noreply.github.com>
2025-06-23 08:48:26 +0200

    refactor: extract duplicate logic into createorUpdateHistoricalSessionNumber method in SessionNumberObserver

--- src/Observers/SessionNumberObserver.php
+++ src/Observers/SessionNumberObserver.php
@@ -2,22 +2,26 @@
 
 namespace TNM\USSD\Observers;
 
-use TNM\USSD\Models\HistoricalSessionNumber;
 use TNM\USSD\Models\SessionNumber;
+use TNM\USSD\Models\HistoricalSessionNumber;
 
 class SessionNumberObserver
 {
     public function created(SessionNumber $sessionNumber)
     {
-        HistoricalSessionNumber::updateOrCreate(['id' => $sessionNumber->getKey()],
-            $sessionNumber->only(['msisdn', 'ussd_session', 'last_screen', 'session_id'])
-        );
+        self::createorUpdateHistoricalSessionNumber($sessionNumber);
     }
 
     public function updated(SessionNumber $sessionNumber)
     {
-        HistoricalSessionNumber::updateOrCreate(['id' => $sessionNumber->getKey()],
-            $sessionNumber->only(['msisdn', 'ussd_session', 'last_screen', 'session_id'])
+        self::createorUpdateHistoricalSessionNumber($sessionNumber);
+    }
+
+    private static function createorUpdateHistoricalSessionNumber(SessionNumber $sessionNumber)
+    {
+        HistoricalSessionNumber::updateOrCreate(
+            ['id' => $sessionNumber->getKey()],
+            $sessionNumber->only(['msisdn', 'session_id', 'ussd_session', 'last_screen'])
         );
     }
 }

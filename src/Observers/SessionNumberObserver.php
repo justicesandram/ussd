7842b263acf3d14b589eccd52935de94d94f3f59
Justice Sandram <108130114+justicesandram@users.noreply.github.com>
2025-06-25 09:12:35 +0200

    refactor: simplify session number history handling

--- src/Observers/SessionNumberObserver.php
+++ src/Observers/SessionNumberObserver.php
@@ -9,19 +9,17 @@ class SessionNumberObserver
 {
     public function created(SessionNumber $sessionNumber)
     {
-        self::createorUpdateHistoricalSessionNumber($sessionNumber);
+        HistoricalSessionNumber::updateOrCreate(
+            ['id' => $sessionNumber->getKey()],
+            $sessionNumber->only(['msisdn', 'ussd_session', 'last_screen', 'session_id'])
+        );
     }
 
     public function updated(SessionNumber $sessionNumber)
     {
-        self::createorUpdateHistoricalSessionNumber($sessionNumber);
-    }
-
-    private static function createorUpdateHistoricalSessionNumber(SessionNumber $sessionNumber)
-    {
         HistoricalSessionNumber::updateOrCreate(
-            ['session_id' => $sessionNumber->getKey()],
-            $sessionNumber->only(['msisdn', 'session_id', 'ussd_session', 'last_screen'])
+            ['id' => $sessionNumber->getKey()],
+            $sessionNumber->only(['msisdn', 'ussd_session', 'last_screen', 'session_id'])
         );
     }
 }

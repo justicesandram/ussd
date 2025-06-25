f1bb50dc4e98a2ab8ca5da1f47bd663938b189b0
Justice Sandram <108130114+justicesandram@users.noreply.github.com>
2025-06-25 10:54:48 +0200

    refactor: add payload relationship to Session model

--- src/Models/Session.php
+++ src/Models/Session.php
@@ -2,9 +2,17 @@
 
 namespace TNM\USSD\Models;
 
+use TNM\USSD\Models\Payload;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class Session extends AbstractSession
 {
     protected $table = 'ussd_sessions';
 
     protected $guarded = [];
+
+    public function payload(): HasMany
+    {
+        return $this->hasMany(Payload::class);
+    }
 }
